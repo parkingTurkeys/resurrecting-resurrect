6395fa6411d41a4ba0183f1139a5dc8d503f4eaf
parking_turkeys <p-t@hackclub.app>
2025-10-30 17:38:22 -0400

    THE CATEGORIES WORK.

--- index.php
+++ index.php
@@ -45,15 +45,20 @@
                 <?php 
                     include "secrets.php";
                     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-                    $mysqli = mysqli_connect("resurrect", "root", "$password", "resurrect", 3306);
+                    $mysqli = mysqli_connect("$host", "$username", "$password", "$host", $port);
                     mysqli_set_charset($mysqli, 'utf8mb4');
-                    $root_categories = mysqli_query($link, "SELECT * FROM Categories WHERE parent=0");
-                    echo '<div class="category">
-                <h3><a href="./index.php?category=N">Announcements</a></h3>
-                <p>
-                    Important news for the whole forum.
-                </p>
-                </div>'
+                    
+                        if (array_key_exists("category", $_GET)) {
+                            $cat = (int)$_GET["category"];
+                            
+                        } else {
+                            $cat = 0;
+                        };
+                        $viewable_categories = mysqli_query($mysqli, "SELECT * FROM Categories WHERE parent=$cat");
+                        $catstoshow_array = $viewable_categories->fetch_all(MYSQLI_ASSOC);
+                        foreach ($catstoshow_array as $cat) {
+                            echo '<div class = "category"><h3><a href="./index.php?category='. $cat["id"] . '">' . $cat["name"] . '</a></h3><p>' . $cat["description"] . '</p></div>';
+                        }
                 ?>
                 <!--
                 <div class="category">
