144ec923fc571d63370aad420658741aa89d662a
parking_turkeys <p-t@hackclub.app>
2025-11-07 20:32:13 -0500

    why can i never remember to update ts message

--- global.php
+++ global.php
@@ -1,4 +1,4 @@
 <?php
-$announce = "This is an unfinished version of a resurrected resurrect. Currently, I am working on properly deploying it [i cannot figure out systemd] and also making posting topics possible!"
+$announce = "This is an unfinished version of a resurrected resurrect. Currently, I am working on properly deploying it [i cannot figure out systemd] and also making some categories locked!"
 
 ?>
